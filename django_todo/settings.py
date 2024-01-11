7eb7391df7e1ea308f04598eb8f4d636b7924e28
Melissa Buckingham <m19b88@gmail.com>
2024-01-11 11:29:25 +0000

    minor changes for heroku deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -15,6 +15,9 @@ if os.path.isfile("env.py"):
     import env
 from pathlib import Path
 
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c)49nz*anp*wrxy3e@p&1_+3mxwo@62ms3gxl5jf&&$rq66n2&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
